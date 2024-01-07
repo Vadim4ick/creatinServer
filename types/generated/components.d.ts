00daf25bc80f912eef82cb2c547d686cedd7c693
Vadim Firulyov <firulvv@mail.ru>
2024-01-07 19:06:17 +0400

    fix reddirect aboutPage

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -178,6 +178,57 @@ export interface SectionsServices extends Schema.Component {
   };
 }
 
+export interface SharedMetaSocial extends Schema.Component {
+  collectionName: 'components_shared_meta_socials';
+  info: {
+    displayName: 'metaSocial';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    socialNetwork: Attribute.Enumeration<['Facebook', 'Twitter']> &
+      Attribute.Required;
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    description: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 65;
+      }>;
+    image: Attribute.Media;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    metaTitle: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaDescription: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 50;
+        maxLength: 160;
+      }>;
+    metaImage: Attribute.Media;
+    metaSocial: Attribute.Component<'shared.meta-social', true>;
+    keywords: Attribute.Text;
+    metaRobots: Attribute.String;
+    structuredData: Attribute.JSON;
+    metaViewport: Attribute.String;
+    canonicalURL: Attribute.String;
+  };
+}
+
 export interface UiLink extends Schema.Component {
   collectionName: 'components_ui_links';
   info: {
@@ -205,6 +256,8 @@ declare module '@strapi/types' {
       'sections.partners': SectionsPartners;
       'sections.section-titles': SectionsSectionTitles;
       'sections.services': SectionsServices;
+      'shared.meta-social': SharedMetaSocial;
+      'shared.seo': SharedSeo;
       'ui.link': UiLink;
     }
   }
